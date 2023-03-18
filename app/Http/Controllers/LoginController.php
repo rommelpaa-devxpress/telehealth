bdedfa196810074ba52062b01c2d5318f35a10b2
Rommel Paa <rommelpaa.devxpress@gmail.com>
2023-03-18 20:53:49 +0800

    :hammer: added new method store update and view

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,8 +2,21 @@
 
 namespace App\Http\Controllers;
 
-
+use Illuminate\Http\Request;
 class LoginController extends Controller
 {
-    //
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function store(Request){
+        // ROMMEL
+    }
+
+
+    public function update();
+
+    public function view()
+
 }
